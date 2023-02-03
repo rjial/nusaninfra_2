d370518e65ffa59a3c037c13b32055b6cfd941ad
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2023-02-04 06:27:02 +0700

    membuat model book

---
+++ database/migrations/2023_02_03_232307_create_books_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("user_id");
+            $table->string("isbn");
+            $table->string("title");
+            $table->string("subtitle");
+            $table->string("author");
+            $table->string("published");
+            $table->string("publisher");
+            $table->integer("pages");
+            $table->string("description");
+            $table->string("website");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
