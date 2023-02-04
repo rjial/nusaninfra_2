38bfb8e94f705fa1c813d782bbcf674897420a79
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2023-02-04 09:08:45 +0700

    fixing

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -15,36 +15,43 @@ class UserController extends Controller
 
     public function register(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            "name" => "required",
-            "email" => "required|email|unique:users",
-            'password' => ['required', 'confirmed', Password::min(8)],
-        ]);
-        if ($validator->fails()) {
-            return response()->json($validator->errors());
-        }
-        $user = new User();
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = Hash::make($request->password);
-        $user->save();
-
-        if ($user->save()) {
-            return response()->json([
-                "message" => "User created",
-                "user"      => $user
-            ], 200);
-        } else {
+        try {
+            $validator = Validator::make($request->all(), [
+                "name" => "required",
+                "email" => "required|email|unique:users",
+                'password' => ['required', 'confirmed', Password::min(8)],
+            ]);
+            if ($validator->fails()) {
+                return response()->json(['message'=> 'Register failed', 'errors' => $validator->errors()], 422);
+            }
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->save();
+    
+            if ($user->save()) {
+                return response()->json([
+                    "message" => "User created",
+                    "user"      => $user
+                ], 200);
+            } else {
+                return response()->json([
+                    "message" => "Register Failed",
+                ], 500);
+            }
+        } catch(\Illuminate\Database\QueryException $queryerr) {
             return response()->json([
                 "message" => "Register Failed",
             ], 500);
         }
+        
     }
     public function login(Request $request)
     {
         if (!Auth::attempt($request->only('email', 'password'))) {
             return response()
-                ->json(['message' => 'Unauthorized'], 401);
+                ->json(['message' => 'Invalid credentials'], 401);
         }
 
         $user = User::where('email', $request['email'])->firstOrFail();
@@ -62,80 +69,5 @@ class UserController extends Controller
         return [
             'message' => 'Logged out'
         ];
-    }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function show(User $user)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(User $user)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, User $user)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(User $user)
-    {
-        //
     }
 }
